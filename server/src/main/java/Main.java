d3baa7dd993466fc46479dbd26923bb71463368d
joshc <joshclan_-c@outlook.com>
2024-02-17 16:48:59 -0700

    starter code added

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import chess.*;
 
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
