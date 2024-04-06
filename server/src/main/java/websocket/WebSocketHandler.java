f03ff8276dade22855ac8b640e7a41ed9ec3d1e4
joshc <joshclan_-c@outlook.com>
2024-04-06 17:01:07 -0600

    Join and JoinObserver work

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,12 +1,14 @@
 package websocket;
 
 import com.google.gson.Gson;
+import com.mysql.cj.ServerVersion;
 import dataAccess.DataAccessException;
 import dataAccess.SQLDAO.SQLAUTHDAO;
 import dataAccess.SQLDAO.SQLGAMEDAO;
 import dataAccess.SQLDAO.SQLUSERDAO;
 import model.AuthData;
 import model.GameData;
+import org.eclipse.jetty.server.Authentication;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
@@ -14,6 +16,7 @@ import webSocketMessages.serverMessages.ErrorMessage;
 import webSocketMessages.serverMessages.LoadGameMessage;
 import webSocketMessages.serverMessages.NotificationMessage;
 import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.JoinObserver;
 import webSocketMessages.userCommands.JoinPlayer;
 import webSocketMessages.userCommands.UserGameCommand;
 
@@ -23,7 +26,14 @@ import java.io.IOException;
 @WebSocket
 public class WebSocketHandler {
 
+    SQLGAMEDAO gamesDatabase = new SQLGAMEDAO();
+    SQLAUTHDAO authDatabase = new SQLAUTHDAO();
+    SQLUSERDAO userDatabase = new SQLUSERDAO();
+
     private final ConnectionManager connections = new ConnectionManager();
+
+    public WebSocketHandler() throws DataAccessException {
+    }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String msg) throws Exception {
@@ -36,17 +46,43 @@ public class WebSocketHandler {
                     JoinPlayer joinPlayerCommand = new Gson().fromJson(msg, JoinPlayer.class);
                     join(conn, command, joinPlayerCommand.getPlayerColor(),joinPlayerCommand.getGameID());
                 }
+                case JOIN_OBSERVER -> {
+                    JoinObserver joinObserver = new Gson().fromJson(msg, JoinObserver.class);
+                    joinObserver(conn,command,joinObserver.getGameID());
+                }
             }
         } else {
             //Connection.sendError(session.getRemote(), "unknown user");
         }
     }
 
+    public void joinObserver(Connection conn, UserGameCommand command,int gameID) throws DataAccessException, IOException {
+
+        try {
+
+            AuthData auth = authDatabase.readAuth(command.getAuthString());
+            if (auth == null){
+                ServerMessage badAuthError = new ErrorMessage("Bad AuthToken");
+                conn.send(new Gson().toJson(badAuthError));
+                return;
+            }
+            GameData gameData = gamesDatabase.readOneGame(gameID);
+            ServerMessage gameMessage = new LoadGameMessage(gameData.getGame());
+            ServerMessage message = new NotificationMessage(auth.username() + " has joined as an observer");
+
+            connections.add(gameID, command.getAuthString(), conn.session);
+            connections.broadcastOthers(gameID, command.getAuthString(), message);
+            conn.send(new Gson().toJson(gameMessage));
+        }
+        catch (DataAccessException ex){
+            if (ex.getStatusCode() == 400){
+                ServerMessage message2 = new ErrorMessage("gameID doesnt exist");
+                conn.send(new Gson().toJson(message2));
+            }
+        }
+    }
+
     public void join(Connection conn, UserGameCommand command, String clientColor, int gameID) throws IOException, DataAccessException {
-        ;
-        SQLGAMEDAO gamesDatabase = new SQLGAMEDAO();
-        SQLAUTHDAO authDatabase = new SQLAUTHDAO();
-        SQLUSERDAO userDatabase = new SQLUSERDAO();
         AuthData auth = authDatabase.readAuth(command.getAuthString());
         ServerMessage message2 = null;
 
@@ -94,14 +130,6 @@ public class WebSocketHandler {
             }
         }
     }
-    private String getOppUser(UserGameCommand command, int gameID) {
-        String excludedUser = "";
-        for (Connection connection: connections.connectionsMap.get(gameID)){
-            if (connection.visitorName != command.getAuthString()){
-                excludedUser = connection.visitorName;
-            }
-        }
-        return excludedUser;
-    }
+
 
 }
