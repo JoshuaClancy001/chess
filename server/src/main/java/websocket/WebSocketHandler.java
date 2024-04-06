1ec9c40a03a6f571bc7e67bae8b5c633c617214c
joshc <joshclan_-c@outlook.com>
2024-04-06 16:26:46 -0600

    4 work

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -42,38 +42,52 @@ public class WebSocketHandler {
         }
     }
 
-    public void join(Connection conn, UserGameCommand command, String clientColor, int gameID) throws IOException, DataAccessException {;
+    public void join(Connection conn, UserGameCommand command, String clientColor, int gameID) throws IOException, DataAccessException {
+        ;
         SQLGAMEDAO gamesDatabase = new SQLGAMEDAO();
         SQLAUTHDAO authDatabase = new SQLAUTHDAO();
         SQLUSERDAO userDatabase = new SQLUSERDAO();
         AuthData auth = authDatabase.readAuth(command.getAuthString());
         ServerMessage message2 = null;
-        GameData game = gamesDatabase.readOneGame(gameID);
-        String whiteUser = game.getWhiteUsername();
-        String blackUser = game.getBlackUsername();
+        try {
+            GameData game = gamesDatabase.readOneGame(gameID);
+            String whiteUser = game.getWhiteUsername();
+            String blackUser = game.getBlackUsername();
 
-        if (clientColor.equals("WHITE") && !whiteUser.equals(auth.username())) {
+            if (clientColor.equals("WHITE")) {
+                if (game.getWhiteUsername() == null) {
+                    ServerMessage message3 = new ErrorMessage("HTTP not called, WHITE is null");
+                    conn.send(new Gson().toJson(message3));
+                    return;
+                }
+            }
+
+            if (clientColor.equals("WHITE") && !whiteUser.equals(auth.username())) {
                 message2 = new ErrorMessage("ClientColor is already taken");
                 conn.send(new Gson().toJson(message2));
                 return;
-        }
-        else if (clientColor.equals("BLACK") && !blackUser.equals(auth.username())){
+            } else if (clientColor.equals("BLACK") && !blackUser.equals(auth.username())) {
                 message2 = new ErrorMessage("Black is already taken");
                 conn.send(new Gson().toJson(message2));
                 return;
+            } else {
+
+                connections.add(gameID, command.getAuthString(), conn.session);
+
+                ServerMessage message = new NotificationMessage("hi" + " joined the game as " + clientColor);
+                ServerMessage message1 = new LoadGameMessage(game.getGame());
+
+                connections.broadcastOthers(gameID, conn.visitorName, message);
+                connections.broadcastSelf(gameID, conn.visitorName, message1);
+            }
         }
-        else {
-
-            connections.add(gameID, command.getAuthString(), conn.session);
-
-            ServerMessage message = new NotificationMessage("hi" + " joined the game as " + clientColor);
-            ServerMessage message1 = new LoadGameMessage(game.getGame());
-
-            connections.broadcastOthers(gameID, conn.visitorName, message);
-            connections.broadcastSelf(gameID, conn.visitorName, message1);
+        catch (DataAccessException ex){
+            if (ex.getStatusCode() == 400){
+                message2 = new ErrorMessage("gameID doesnt exist");
+                conn.send(new Gson().toJson(message2));
+            }
         }
     }
-
     private String getOppUser(UserGameCommand command, int gameID) {
         String excludedUser = "";
         for (Connection connection: connections.connectionsMap.get(gameID)){
